d816a392c7e643ad1be26399f0290291f7a1d5ce
Martin Skarzynski <marskar@gmail.com>
2018-12-17 23:59:00 -0500

    update setup.py and cookiecutter.json

--- {{ cookiecutter.repo }}/setup.py
+++ {{ cookiecutter.repo }}/setup.py
@@ -10,7 +10,13 @@ setuptools.setup(
     long_description=long_desc,
     long_description_content_type="text/x-rst",
     author='{{ cookiecutter.author }}',
+    url='https://www.github.com/{{ cookiecutter.username }}/{{ cookiecutter.repo }}/',
     license='{% if cookiecutter.license == 'MIT' %}MIT{% elif cookiecutter.license == 'BSD-3-Clause' %}BSD-3{% endif %}',
     packages=setuptools.find_packages(where='src'),
     package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
