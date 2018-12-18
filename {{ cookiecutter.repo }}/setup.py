fd47dc13f592d36f747eb7f2cdc4c4de534ba801
Martin Skarzynski <marskar@gmail.com>
2018-12-17 23:46:47 -0500

    update setup.py

--- {{ cookiecutter.repo }}/setup.py
+++ {{ cookiecutter.repo }}/setup.py
@@ -1,10 +1,16 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
+with open("README.rst", "r") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='{{ cookiecutter.repo }}',
+    version='{{ cookiecutter.version }}',
     description='{{ cookiecutter.description }}',
+    long_description=long_desc,
+    long_description_content_type="text/x-rst",
     author='{{ cookiecutter.author }}',
     license='{% if cookiecutter.license == 'MIT' %}MIT{% elif cookiecutter.license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
 )
