1af4af4d5de320974c095b252c6e04d126ed97ea
Martin Skarzynski <marskar@gmail.com>
2018-12-23 17:52:53 -0500

    rearrange src

--- {{ cookiecutter.repo }}/{{cookiecutter.repo}}/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for {{ cookiecutter.project }}."""
-
-__author__ = """{{ cookiecutter.author }}"""
-__version__ = '{{ cookiecutter.version }}'
-
--- {{ cookiecutter.repo }}/{{cookiecutter.repo}}/src/{{cookiecutter.repo}}.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-"""Main module."""
-
-def {{cookiecutter.repo}}(arg1: str) -> None:
-    print(arg1)
